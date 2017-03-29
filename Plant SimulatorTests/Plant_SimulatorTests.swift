b018e848e8a71ff445bd50b9a591a7e00cca0b62
梁思地 <liangsidi@sidi-liangs-air.la.local>
2017-03-29 13:40:16 -0400

    Initial Commit

---
+++ Plant SimulatorTests/Plant_SimulatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Plant_SimulatorTests.swift
+//  Plant SimulatorTests
+//
+//  Created by 梁思地 on 2017/3/29.
+//  Copyright © 2017年 梁思地. All rights reserved.
+//
+
+import XCTest
+@testable import Plant_Simulator
+
+class Plant_SimulatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
